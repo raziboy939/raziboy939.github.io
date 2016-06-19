da3a6c5b8999f8087a62cb8c65cbe591eb4153e4
Rahman Raza <rraza@ucdavis.edu>
2016-06-18 21:23:18 -0700

    resume

---
+++ Resume/contact_me.php
@@ -0,0 +1,30 @@
+<?php
+// check if fields passed are empty
+
+if(empty($_POST['name'])  		||
+   empty($_POST['email']) 		||
+   empty($_POST['message'])	||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+	echo "No arguments Provided!";
+	return false;
+   }
+   
+
+	
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$message = $_POST['message'];
+	
+// create email body and send it	
+$to = 'rraza@ucdavis.edu'; // PUT YOUR EMAIL ADDRESS HERE
+$email_subject = "resume contact form:  $name"; // EDIT THE EMAIL SUBJECT LINE HERE
+$email_body = "You have received a new message from your website's contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
+$headers = "From: rraza@ucdavis.edu\n";
+$headers .= "Reply-To: $email_address";	
+
+	$headers .= "Return-Path: rraza@ucdavis.edu\r\n";
+	$headers .= "X-Mailer: PHP \r\n";
+mail($to,$email_subject,$email_body,$headers);
+return true;			
+?>
